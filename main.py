7ee68c7763ea9b54c9f5067db5b0b0ae7f2964e3
Rolando Velez <rolandovlz@gmail.com>
2025-09-06 22:54:53 -0700

    Added Gemini API client

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
 def main():
-    print("Hello from bdev-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents='Why is the sky blue?'
+    )
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
