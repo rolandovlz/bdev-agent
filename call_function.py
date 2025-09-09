b68d9308e0d1ea1e65dad50aa98accb3c678f9a8
Rolando Velez <rolandovlz@gmail.com>
2025-09-08 19:15:34 -0700

    added function calling

--- call_function.py
+++ call_function.py
@@ -1,5 +1,8 @@
 from google.genai import types
-
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 from functions.schemas import *
 
 available_functions = types.Tool(
@@ -10,3 +13,43 @@ available_functions = types.Tool(
         schema_write_file,
     ]
 )
+
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_name = function_call_part.name
+    args = function_call_part.args
+    res = None
+    match (function_call_part.name):
+        case "get_files_info":
+            res = get_files_info("./calculator", **args)
+        case "get_file_content":
+            res = get_file_content("./calculator", **args)
+        case "write_file":
+            res = write_file("./calculator", **args)
+        case "run_python_file":
+            res = run_python_file("./calculator", **args)
+        case _:
+            return types.Content(
+                role="tool",
+                parts=[
+                    types.Part.from_function_response(
+                        name=function_name,
+                        response={"error": f"Unknown function: {function_name}"}
+                    )
+                ]
+            )
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": res}
+            )
+        ]
+    )
+
--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 from prompts import system_prompt
-from functions.schemas import schema_get_files_info, schema_get_file_content, schema_write_file, schema_run_python_file
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 
 def main():
@@ -54,8 +53,11 @@ def generate_content(client, messages, verbose):
         return response.text
 
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-
+        function_call_result = call_function(function_call_part)
+        if not function_call_result.parts[0].function_response.response:
+            raise Exception("Error: Couldn't get a response from the function call")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
 
 if __name__ == "__main__":
     main()
